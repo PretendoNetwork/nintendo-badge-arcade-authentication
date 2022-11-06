af1e0705611463731f250912cc2ddf49dd8cbd0a
Daniel López Guimaraes <danielectra@outlook.com>
2022-11-06 17:59:30 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Nintendo Badge Arcade authentication
+
+The NEX authentication server for Nintendo Badge Arcade.
+
+## Current status
+
+The server "works", it gets requests and sends replies, but the Nintendo 3DS errors about "maintenance" of the servers.
+
+## Credits
+
+The codebase of the server is based on Pretendo Network's (Wii U Chat authentication)[https://github.com/PretendoNetwork/wiiu-chat-authentication] server.
+
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	nex "github.com/PretendoNetwork/nex-go"
+	"github.com/PretendoNetwork/nex-protocols-common-go/authentication"
+)
+
+var nexServer *nex.Server
+
+func main() {
+	nexServer = nex.NewServer()
+	nexServer.SetPrudpVersion(1)
+	nexServer.SetNexVersion(2)
+	nexServer.SetKerberosKeySize(32)
+	nexServer.SetKerberosPassword(os.Getenv("KERBEROS_PASSWORD"))
+	nexServer.SetAccessKey("82d5962d")
+
+	nexServer.On("Data", func(packet *nex.PacketV1) {
+		request := packet.RMCRequest()
+
+		fmt.Println("==Badge Arcade - Auth==")
+		fmt.Printf("Protocol ID: %#v\n", request.ProtocolID())
+		fmt.Printf("Method ID: %#v\n", request.MethodID())
+		fmt.Println("====================")
+	})
+
+	authenticationProtocol := authentication.NewCommonAuthenticationProtocol(nexServer)
+
+	secureStationURL := nex.NewStationURL("")
+	secureStationURL.SetScheme("prudps")
+	secureStationURL.SetAddress(os.Getenv("SECURE_SERVER_LOCATION"))
+	secureStationURL.SetPort(os.Getenv("SECURE_SERVER_PORT"))
+	secureStationURL.SetCID("1")
+	secureStationURL.SetPID("2")
+	secureStationURL.SetSID("1")
+	secureStationURL.SetStream("10")
+	secureStationURL.SetType("2")
+
+	authenticationProtocol.SetSecureStationURL(secureStationURL)
+	authenticationProtocol.SetBuildName("Badge Arcade Auth")
+	authenticationProtocol.SetPasswordFromPIDFunction(passwordFromPID)
+
+	nexServer.Listen(":59400")
+}
